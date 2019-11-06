41a240d3e94edff1fd2627abb6a8d34031ac7364
LIManuel <leonmalisov@gmail.com>
2019-11-05 20:13:15 -0500

    updated menu-items to navigate withRouter HOC (higher order component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import { Switch, Route } from "react-router-dom";
+
+
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component"
 
+const HatsPage = () => (
+	<div>
+		<h1>Hats Page</h1>
+	</div>
+);
+
 function App() {
-  return (
-    <div className="App">
-      <HomePage />
-    </div>
-  );
+	return (
+		<div>
+			<Switch> {/*The moment switch sees something match the path (as long as a route gets rendered) it renders only that. Useful if we don't want to accidentally render multiple components*/}
+				{/* when path is at the base url, open the homepage. "exact" means the path has to be exactly '/'*/}
+				<Route exact path='/' component={HomePage} />
+				<Route path='/hats' component={HatsPage} />
+			</Switch>
+		</div>
+	)
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <HomePage />
+//     </div>
+//   );
+// }
 
 export default App;
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import { withRouter } from "react-router-dom";
 import './menu-item.styles.scss'
 
-const MenuItem = ({title, imageUrl, size}) => ( //dynamically generating title, destructured. Same thing as having (props here), and this.props later in the jsx
-	<div className={`${size} menu-item`} >
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => ( //dynamically generating title, destructured. Same thing as having (props here), and this.props later in the jsx
+	<div className={`${size} menu-item`} 
+	onClick={() => history.push(`${match.url}${linkUrl}`)}>
 		<div className="background-image" style={{backgroundImage: `url(${imageUrl})` }} />
 		<div className = "content">
 			<h1 className = "title">{title.toUpperCase()}</h1>
@@ -12,4 +13,4 @@ const MenuItem = ({title, imageUrl, size}) => ( //dynamically generating title, 
 	</div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem); //it will return a super-powered menu component with access to router props
