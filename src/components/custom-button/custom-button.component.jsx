515779babb2412189a7f86286eeff28c501b22c6
LIManuel <leonmalisov@gmail.com>
2019-11-10 23:44:11 -0500

    added add to cart buttons for shop items on collections page

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import "./custom-button.styles.scss";
 
-const CustomButton = ({children, isGoogleSignIn, ...otherProps}) => (
-	<button className={`${isGoogleSignIn ? "google-sign-in" : "nope"} custom-button`} {...otherProps}>
+const CustomButton = ({children, isGoogleSignIn, inverted, ...otherProps}) => (
+	<button className={`${inverted ? "inverted" : "nope"}
+		${isGoogleSignIn ? "google-sign-in" : "nope"} custom-button`} {...otherProps}>
 		{children}
 	</button>
 );
